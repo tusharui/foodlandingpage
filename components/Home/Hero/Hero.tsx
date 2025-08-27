4c192e7793f37cc1990400a4e0d97d3d35d82dcd
Tushar <tusharanshu18@gmail.com>
2025-08-27 14:09:27 +0530

    added image section added icons of google play and app

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { FaLocationArrow } from 'react-icons/fa'
+import { FaApple, FaGooglePlay, FaLocationArrow } from 'react-icons/fa'
+import { FaGooglePay } from 'react-icons/fa6'
 import { GrMapLocation } from 'react-icons/gr'
+import Image from 'next/image'
 
 const Hero = () => {
     return (
@@ -23,23 +25,62 @@ const Hero = () => {
                             <div className='px-4 text-gray-500'>
                                 <GrMapLocation className='w-5 h-5 text-black' />
                             </div>
-                            <input type="text" placeholder='Enter your address' className='flex-1 px-2 text-sm text-black bg-transparent outline-none h-full ' />
-                            <button className='bg-gray-900 text-white px-4  h-full hover:bg-gray-800 flex items-center justify-center cursor-pointer'>
-  <FaLocationArrow className='w-4 h-4' />
-</button>
+                            <input type="text" placeholder='Enter your address' className='flex-1 px-2 text-sm text-black bg-transparent outline-none h-full  ' />
+                            <button className='bg-gray-900 text-white px-4  h-full hover:bg-gray-800 flex items-center justify-center cursor-pointer rounded-md '>
+                                <FaLocationArrow className='w-6 h-6' />
+                            </button>
 
 
 
                         </div>
 
                     </div>
+                    {/* download text */}
+                    <p className='text-gray-700 dark:text-gray-400 text-sm mb-6'>
+                        Apps available to download on 
+                    </p>
+                    {/*   app store button */}
+                    <div className='flex  space-x-4 '>
+                        <a href="#_" className='flex items-center group border border-gray-400 px-4 py-3 rounded-md hover:bg-gray-950 trnasition-all duration-300'>
+
+                            <FaApple className='text-2xl mr-2 group-hover:text-white transition-all duration-300'/>.
+                            <div>
+                                <p className='text-xs group-hover:text-white transition-all duration-300 '>Download on the </p>
+                                <p className='text-sm font-semibold group-hover:text-whote transition-all duration-300'> App Store</p>
+                            </div>
+                        </a>
+{/*   google  store button */}
+                        <a href="#_" className='flex items-center group border border-gray-400 px-4 py-3 rounded-md hover:bg-gray-950 trnasition-all duration-300'>
+
+                            <FaGooglePlay className='text-2xl mr-2 group-hover:text-white transition-all duration-300'/>.
+                            <div>
+                                <p className='text-xs group-hover:text-white transition-all duration-300 '>Download on the </p>
+                                <p className='text-sm font-semibold group-hover:text-whote transition-all duration-300'> Google Play</p>
+                            </div>
+                        </a>
+
+                 </div>
+                    </div>
+
+                     {/* image content  */}
+                <div className='mx-auto hidden xl:block  justify-end'> 
+                    <Image src="/images/hero.png"  alt = "image" width ={500}  height ={500}/>
+                         
+                        </div>
+
                 </div>
 
-                {/* image content  */}
-                <div> </div>
+
+
+
+
+
+
+
+               
 
             </div>
-        </div>
+        
     )
 }
 
